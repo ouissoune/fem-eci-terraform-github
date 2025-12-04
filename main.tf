982486f4905c927a28debbe7e45e6616bc22257f
ouissoune ilyas <ilyasyassine41@gmail.com>
2025-12-04 09:20:10 +0100

    fix/fix moved block reference (#5)
    * feat: added azure repos
    
    * fix: comments resolved
    
    * refactor: added new tags and replace new ones and change the names of the projects
    
    * added moved blocks to prevent terraform destory
    
    * fix
    
    * fix: moved block should reference the actual resource and not tyhe variable
    
    ---------
    
    Co-authored-by: kilmer-i <ilyas.ouissoune@usmba.ac.ma>

--- main.tf
+++ main.tf
@@ -13,3 +13,38 @@ module "repository" {
   topics             = each.value.topics
   visibility         = each.value.visibility
 }
+
+moved {
+  from = module.repository["fem-eci-terraform-github"]
+  to   = module.repository["kilmer-mba-terraform-github"]
+}
+
+moved {
+  from = module.repository["fem-eci-terraform-azure-cluster"]
+  to   = module.repository["kilmer-mba-terraform-azure-cluster"]
+}
+
+moved {
+  from = module.repository["fem-eci-terraform-azure-network"]
+  to   = module.repository["kilmer-mba-terraform-azure-network"]
+}
+
+moved {
+  from = module.repository["fem-eci-terraform-azure-serviceaccount"]
+  to   = module.repository["kilmer-mba-terraform-azure-serviceaccount"]
+}
+
+moved {
+  from = module.repository["fem-eci-terraform-azure-keyvault"]
+  to   = module.repository["kilmer-mba-terraform-azure-keyvault"]
+}
+
+moved {
+  from = module.repository["fem-eci-terraform-azure-rg"]
+  to   = module.repository["kilmer-mba-terraform-azure-rg"]
+}
+
+moved {
+  from = module.repository["fem-eci-terraform-tfe"]
+  to   = module.repository["kilmer-mba-terraform-tfe"]
+}
