f4337f74f29e79245c5c9107d6adc13bdd4cfb52
ouissoune ilyas <ilyasyassine41@gmail.com>
2025-12-04 08:52:35 +0100

    refactor/name migration (#2)
    * feat: added azure repos
    
    * fix: comments resolved
    
    * refactor: added new tags and replace new ones and change the names of the projects
    
    ---------
    
    Co-authored-by: kilmer-i <ilyas.ouissoune@usmba.ac.ma>

--- locals.tf
+++ locals.tf
@@ -1,34 +1,60 @@
 locals {
   repos = {
-    "fem-eci-terraform-tfe" = {
+    "kilmer-mba-terraform-tfe" = {
       description        = "Automation for Terraform Enterprise"
       gitignore_template = "Terraform"
-      name               = "fem-eci-terraform-tfe"
-      topics             = ["frontend-masters", "terraform"]
+      name               = "kilmer-mba-terraform-tfe"
+      topics             = ["kilmerx", "terraform", "microservices", "iac", "spring-boot"]
       visibility         = "public"
     }
 
-    "fem-eci-terraform-github" = {
+    "kilmer-mba-terraform-github" = {
       description        = "Automation for GitHub"
       gitignore_template = "Terraform"
-      name               = "fem-eci-terraform-github"
-      topics             = ["frontend-masters", "terraform"]
+      name               = "kilmer-mba-terraform-github"
+      topics             = ["kilmerx", "terraform", "microservices", "iac", "spring-boot"]
       visibility         = "public"
     }
 
-    "fem-eci-terraform-azure-network" = {
+    "kilmer-mba-terraform-azure-rg" = {
+      description        = "Automation for Azure Resource Groups"
+      gitignore_template = "Terraform"
+      name               = "kilmer-mba-terraform-azure-rg"
+      topics             = ["kilmerx", "terraform", "microservices", "iac", "spring-boot"]
+      visibility         = "public"
+    }
+
+    "kilmer-mba-terraform-azure-keyvault" = {
+      description        = "Automation for Azure KeyVault"
+      gitignore_template = "Terraform"
+      name               = "kilmer-mba-terraform-azure-keyvault"
+      topics             = ["kilmerx", "terraform", "microservices", "iac", "spring-boot"]
+      visibility         = "public"
+    }
+
+    "kilmer-mba-terraform-azure-serviceaccount" = {
+      description        = "Automation for Azure Service Accounts"
+      gitignore_template = "Terraform"
+      name               = "kilmer-mba-terraform-azure-serviceaccount"
+      topics             = ["kilmerx", "terraform", "microservices", "iac", "spring-boot"]
+      visibility         = "public"
+    }
+    
+    
+    
+    "kilmer-mba-terraform-azure-network" = {
       description        = "Automation for Azure Network"
       gitignore_template = "Terraform"
-      name               = "fem-eci-terraform-azure-network"
-      topics             = ["frontend-masters", "terraform"]
+      name               = "kilmer-mba-terraform-azure-network"
+      topics             = ["kilmerx", "terraform", "microservices", "iac", "spring-boot"]
       visibility         = "public"
     }
 
-    "fem-eci-terraform-azure-cluster" = {
+    "kilmer-mba-terraform-azure-cluster" = {
       description        = "Automation for Azure Cluster"
       gitignore_template = "Terraform"
-      name               = "fem-eci-terraform-azure-cluster"
-      topics             = ["frontend-masters", "terraform"]
+      name               = "kilmer-mba-terraform-azure-cluster"
+      topics             = ["kilmerx", "terraform", "microservices", "iac", "spring-boot"]
       visibility         = "public"
     }
 
